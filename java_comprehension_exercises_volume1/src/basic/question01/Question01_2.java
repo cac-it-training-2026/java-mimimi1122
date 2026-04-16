3d62983bdd0b2a1182c54dea4e13162042517153
mnamba <mnamba@C010423475.zipangu.cac.co.jp>
2026-04-16 10:41:32 +0900

    Question01

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
@@ -7,8 +7,13 @@ package basic.question01;
 public class Question01_2 {
 
 	public static void main(String[] args) {
-		
-		// TODO:ここに実装
+		int applePrice = 100;
+		String stationery = "鉛筆";
+		int bananaPrice = applePrice;
+		String writingUtensils = stationery;
+		System.out.println(applePrice);
+		System.out.println(bananaPrice);
+		System.out.println(writingUtensils);
 
 	}
 
