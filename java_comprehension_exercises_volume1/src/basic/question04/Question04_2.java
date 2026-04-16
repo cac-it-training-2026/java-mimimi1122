5bbe213d1910ae0f01e2fcdcfc9bb6eb0d6a595f
mnamba <mnamba@C010423475.zipangu.cac.co.jp>
2026-04-16 11:50:19 +0900

    Question04

--- java_comprehension_exercises_volume1/src/basic/question04/Question04_2.java
+++ java_comprehension_exercises_volume1/src/basic/question04/Question04_2.java
@@ -1,6 +1,8 @@
 package basic.question04;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第7章 配列
@@ -10,8 +12,14 @@ public class Question04_2 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
-		
+		int[] ageGroup = { 23, 33, 26, 21, 25, 22 };
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		int temporaryAge = Integer.parseInt(reader.readLine());
+
+		ageGroup[3] = temporaryAge;
+		System.out.println(ageGroup[3]);
+		System.out.println(ageGroup.length);
 	}
 
 }
